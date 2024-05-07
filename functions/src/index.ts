8e35d753229aede9ad633d5adf73831ebd683971
Thy Tran <22521461@gm.uit.edu.vn>
2024-05-07 19:05:23 +0700

    refactor: remove unused functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,15 +1,15 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions and triggers.
 import * as functions from "firebase-functions";
-
 // The Firebase Admin SDK to access Firestore.
 import * as admin from "firebase-admin";
 import { getFirestore, FieldValue } from "firebase-admin/firestore";
-
 // Crypto
 import * as crypto from "node:crypto";
 
 const NUMBER_OF_DIGIT = 5;
+
 // App init
+
 admin.initializeApp();
 const db = getFirestore();
 
@@ -34,6 +34,7 @@ export const addTimeCreatedToPost = functions.firestore
   });
 
 // add lastModified when a post is edited
+
 export const addLastModifiedToEditedPost = functions.firestore
   .document("/Community/{communityID}/Post/{postID}")
   .onUpdate(async (change, context) => {
@@ -48,7 +49,7 @@ export const addLastModifiedToEditedPost = functions.firestore
     return await postRef.update({
       lastModified: FieldValue.serverTimestamp(),
     });
-  })
+  });
 
 // add sample - announcement category everytime a new community is created
 
@@ -68,11 +69,13 @@ export const addSampleCategory = functions.firestore
   });
 
 // add user to their default department
+
 export const addUserDefaultDepartment = functions.firestore
   .document("/User/{documentId}")
   .onCreate(async (snapshot, context) => {
     const department = snapshot.get("department");
     const userID = context.params.documentId;
+
     const communityQuerySnapshot = await db
       .collection("Community")
       .where("department", "==", department)
@@ -83,65 +86,23 @@ export const addUserDefaultDepartment = functions.firestore
       return;
     }
 
-    const community = {
-      communityId: communityQuerySnapshot.docs[0].id,
-      name: communityQuerySnapshot.docs[0].data().name,
-      department: communityQuerySnapshot.docs[0].data().department,
-      description: communityQuerySnapshot.docs[0].data().description,
-    };
+    // const community = {
+    //   communityId: communityQuerySnapshot.docs[0].id,
+    //   name: communityQuerySnapshot.docs[0].data().name,
+    //   department: communityQuerySnapshot.docs[0].data().department,
+    //   description: communityQuerySnapshot.docs[0].data().description,
+    // };
 
-    // Reference to the CommunityMember document for this user
-    const communityMemberRef = db.collection("CommunityMember").doc(userID);
+    const communityRef = db
+      .collection("Community")
+      .doc(communityQuerySnapshot.docs[0].id);
 
-    return communityMemberRef.set(
-      {
-        communities: FieldValue.arrayUnion(community),
-      },
-      { merge: true }
-    );
+    return communityRef.update({
+      userList: FieldValue.arrayUnion(userID),
+    });
   });
 
-// add community admin
-export const addAdminToCommunity = functions.firestore
-  .document("/Community/{documentId}")
-  .onCreate(async (snapshot, context) => {
-    const userID = snapshot.get("adminList")[0];
-
-    const community = {
-      communityId: snapshot.id,
-      name: snapshot.data().name,
-      department: snapshot.data().department,
-      description: snapshot.data().description,
-    };
-
-    // Reference to the CommunityMember document for this user
-    const communityMemberRef = db.collection("CommunityMember").doc(userID);
-
-    return communityMemberRef.set(
-      {
-        communities: FieldValue.arrayUnion(community),
-      },
-      { merge: true }
-    );
-  });
-
-// create new communitymember document
-export const createCommunityMember = functions.auth
-  .user()
-  .onCreate(async (user) => {
-    const communityMemberRef = db.collection("CommunityMember").doc(user.uid);
-    try {
-      await communityMemberRef.set(
-        {
-          communities: [],
-        },
-        { merge: true }
-      );
-      console.log("CommunityMember document created for user:", user.uid);
-    } catch (error) {
-      console.error("Error creating CommunityMember document:", error);
-    }
-  });
+// generate invite code for communities
 
 export const generateCommunityCode = functions.firestore
   .document("Community/{communityID}")
@@ -173,3 +134,45 @@ function generateAlphanumericCode(length: number) {
   }
   return result;
 }
+
+// create new communitymember document
+// export const createCommunityMember = functions.auth
+//   .user()
+//   .onCreate(async (user) => {
+//     const communityMemberRef = db.collection("CommunityMember").doc(user.uid);
+//     try {
+//       await communityMemberRef.set(
+//         {
+//           communities: [],
+//         },
+//         { merge: true }
+//       );
+//       console.log("CommunityMember document created for user:", user.uid);
+//     } catch (error) {
+//       console.error("Error creating CommunityMember document:", error);
+//     }
+//   });
+
+// add community admin
+// export const addAdminToCommunity = functions.firestore
+//   .document("/Community/{documentId}")
+//   .onCreate(async (snapshot, context) => {
+//     const userID = snapshot.get("adminList")[0];
+
+//     const community = {
+//       communityId: snapshot.id,
+//       name: snapshot.data().name,
+//       department: snapshot.data().department,
+//       description: snapshot.data().description,
+//     };
+
+//     // Reference to the CommunityMember document for this user
+//     const communityMemberRef = db.collection("CommunityMember").doc(userID);
+
+//     return communityMemberRef.set(
+//       {
+//         communities: FieldValue.arrayUnion(community),
+//       },
+//       { merge: true }
+//     );
+//   });
